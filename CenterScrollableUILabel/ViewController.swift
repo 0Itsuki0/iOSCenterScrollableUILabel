df1a8483850f8ab128e7fddedd48176e34386359
000itsuki000 <itsuki.h@collabo-style.co.jp>
2024-04-05 17:25:21 +0900

    Centered Scrollable UILabel Demo

--- CenterScrollableUILabel/ViewController.swift
+++ CenterScrollableUILabel/ViewController.swift
@@ -9,10 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var centerYConstraint: NSLayoutConstraint!
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print(label.intrinsicContentSize.height)
+        print(scrollView.frame.height)
+        self.centerYConstraint.isActive = (label.intrinsicContentSize.height < scrollView.frame.height)
+
+    }
+
 
 
 }
